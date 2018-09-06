3368370128707fec10bbea9d432a401804f71ca7
Colton Clemmer <cole.clemmer@gmail.com>
2018-09-06 10:29:10 -0700

    WIP

--- client/purchase/viewModel.js
+++ client/purchase/viewModel.js
@@ -1,6 +1,8 @@
 import api from 'api'
 import ko from 'knockout'
 
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 export default class {
   constructor() {
     this.id = 0
@@ -9,11 +11,22 @@ export default class {
   }
 
   async init() {
-    this.purchase = await api.get('/api/purchase/' + this.id)
+    const p = await api.get('/api/purchase/' + this.id)
+    this.purchase = {
+      date: ko.observable(p.date),
+      title: ko.observable(p.title),
+      description: ko.observable(p.description),
+      amount: ko.observable(p.amount),
+      reference: ko.observable(p.reference)
+    }
+    this.computedDate = ko.pureComputed(() => new Date(this.purchase.date()))
+    this.purchase.date(
+      months[this.computedDate().getMonth()] + ' ' + this.computedDate().getDate() + ', ' + this.computedDate().getFullYear()
+    )
     this.ready(true)
   }
 
   save() {
-
+    this.purchase.date(this.computedDate)
   }
 }
